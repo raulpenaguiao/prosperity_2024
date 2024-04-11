c7b9279bbb3feff28b0c3b60bcb2a20dbbcb21fe
raulpenaguiao <92503059+raulpenaguiao@users.noreply.github.com>
2024-04-11 08:22:19 +0200

    updated trader

--- stage1/trader.py
+++ stage1/trader.py
@@ -10,22 +10,30 @@ class Trader:
 
 				# Orders to be placed on exchange matching engine
         result = {}
+
+        products = ["STARFRUIT", "AMETHYSTS"]
+        limits = {p:20 for p in products}
+        means = {products[0]:5058.63895, product[1]:9999.99975} #Using data from day0
+        stdevs = {products[0]:1.496228, product[1]:11.717389} #Using data from day0
+        
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
+            acceptable_price_max = means[product] + 2 * stdevs[product]
+            acceptable_price_min = means[product] - 2 * stdevs[product]
+            print("Acceptable price min : " + str(acceptable_price_min))
+            print("Acceptable price max : " + str(acceptable_price_max))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
+                if int(best_ask) < acceptable_price_max:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
+                if int(best_bid) > acceptable_price_min:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
                     orders.append(Order(product, best_bid, -best_bid_amount))
             
